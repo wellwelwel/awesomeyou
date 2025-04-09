f36541a57cc4d9bbf97ccab009831d38231da60c
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2025-04-09 08:30:28 -0300

    refactor(maintainer): simplify score

--- src/pages/_dynamic/maintainer/_project.tsx
+++ src/pages/_dynamic/maintainer/_project.tsx
@@ -16,7 +16,6 @@ import {
 } from 'lucide-react';
 import { SafeLink } from '@site/src/components/SafeLink';
 import { Context } from '@site/src/contexts/Maintainer';
-import { getScore } from '@site/src/helpers/get-score';
 import { localeNumber } from '@site/src/helpers/services/stats/set-result';
 
 type Props = ProjectOptions & {
@@ -30,6 +29,7 @@ export const Project: FC<Props> = (project) => {
   const { maintainer } = useContext(Context);
   const { name, username } = maintainer;
   const { stats, organization } = project;
+  const { score } = stats;
   const isAuthor = project.isAuthor ? 'criou' : 'mantém';
   const isLanguage =
     project.categories?.includes('language') && 'a linguagem de programação';
@@ -74,21 +74,6 @@ export const Project: FC<Props> = (project) => {
 
   const totalDownloads =
     (stats.chocolatey?.value || 0) + (stats.vscode?.value || 0);
-
-  const score = getScore({
-    chocolatey: stats.chocolatey?.value,
-    closedIssues: stats.closedIssues?.value,
-    commits: stats.commits,
-    contributors: stats.contributors?.value,
-    forks: stats.forks?.value,
-    homebrew: stats.homebrew?.value,
-    issues: stats.issues?.value,
-    npm: stats.npm?.value,
-    pypi: stats.pypi?.value,
-    repositoryDependents: stats.repositoryDependents?.value,
-    stars: stats.stars?.value,
-    vscode: stats.vscode?.value,
-  });
 
   return (
     <section className='project'>
