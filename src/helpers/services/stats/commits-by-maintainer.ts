0041922712f654913218878749fe95e60ef57893
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2025-04-14 02:01:47 -0300

    chore: add maintainer commit counter fallback

--- src/helpers/services/stats/commits-by-maintainer.ts
+++ src/helpers/services/stats/commits-by-maintainer.ts
@@ -4,17 +4,63 @@
  *--------------------------------------------------------------------------------------------*/
 
 import type { StatsPropos } from '@site/src/@types/projects.js';
-import { setResult } from './set-result.js';
+import { env } from 'node:process';
+import { localeNumber, setResult } from './set-result.js';
 
+const token = env.GITHUB_TOKEN;
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const getCommitsByMaintainerManually = async (
+  organization: string,
+  repository: string,
+  maintainer: string
+): Promise<StatsPropos> => {
+  const perPage = 100;
+  const headers = token ? { Authorization: `Bearer ${token}` } : undefined;
+
+  let page = 1;
+  let totalCommits = 0;
+
+  while (true) {
+    const response = await fetch(
+      `https://api.github.com/repos/${organization}/${repository}/commits?author=${maintainer}&per_page=${perPage}&page=${page}`,
+      { headers }
+    );
+
+    if (!response.ok) throw new Error('GitHub API error');
+
+    const commits = await response.json();
+
+    totalCommits += commits.length;
+
+    if (commits.length < 100) break;
+
+    page++;
+  }
+
+  const results = {
+    value: totalCommits,
+    label: localeNumber(totalCommits),
+  };
+
+  console.log(
+    results,
+    maintainer,
+    `${organization}/${repository}`,
+    '(Fallback)'
+  );
+
+  return results;
+};
 
 export const commitsByMaintainer = async (
   organization: string,
   repository: string,
   maintainer: string
 ): Promise<StatsPropos> => {
-  const maxRetries = 60;
-  const retryDelay = 1000 + 100;
+  const maxRetries = 10;
+  const lag = 10;
+  const retryDelay = 1000 + lag;
 
   let processed: StatsPropos | undefined;
   let attempts = 0;
@@ -38,9 +84,20 @@ export const commitsByMaintainer = async (
   }
 
   if (!processed || processed.value === 0) {
-    throw new Error(
-      `Failed to generate the number of commits for ${maintainer} in ${organization}/${repository}`
-    );
+    try {
+      processed = await getCommitsByMaintainerManually(
+        organization,
+        repository,
+        maintainer
+      );
+    } catch (error) {
+      if (error instanceof Error) console.log(error.message);
+    }
+
+    if (!processed || processed.value === 0)
+      throw new Error(
+        `Failed to generate the number of commits for ${maintainer} in ${organization}/${repository}`
+      );
   }
 
   return processed;
